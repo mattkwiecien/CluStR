68ce62e471f0fff312e66e851b22d4222a3e6ed8
Matt Kwiecien <mattkwiecien@gmail.com>
2022-06-17 14:07:34 -0700

    Adding a simple setup.py file.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='clustr',
+    version='1.0',
+    description='This package calculates various scaling relations from cluster catalogs.',
+    author='Paige, Jose, Spencer',
+    author_email='',
+    url='https://www.python.org/sigs/distutils-sig/',
+    packages=['clustr']
+)
